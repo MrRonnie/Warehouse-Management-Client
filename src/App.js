8c8f47e47bc9f6209dde728bca95706e0d772467
MrRonnie <ronymaza55@gmail.com>
2022-05-07 19:43:30 +0600

    Header route connected

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./components/Home/Home/Home";
 import Header from "./components/Shared/Header/Header";
 import Footer from "./components/Shared/Footer/Footer";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <>
       <Header></Header>
-      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home></Home>} />
+        <Route path="/home" element={<Home></Home>} />
+      </Routes>
       <Footer></Footer>
-    </div>
+    </>
   );
 }
 
