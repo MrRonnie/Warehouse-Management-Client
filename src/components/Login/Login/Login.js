a77a73aabd7faa46e56dca14c9ca60892dfcf9bb
MrRonnie <ronymaza55@gmail.com>
2022-05-09 11:05:22 +0600

    Email password based authentication with react firebase hooks

--- src/components/Login/Login/Login.js
+++ src/components/Login/Login/Login.js
@@ -1,11 +1,76 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Button, Form } from "react-bootstrap";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../../firebase.init";
 
 const Login = () => {
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const navigate = useNavigate();
+
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    console.log(email, password);
+
+    signInWithEmailAndPassword(email, password);
+  };
+
+  if (user) {
+    navigate("/home");
+  }
+
+  const navigateRegister = (event) => {
+    navigate("/register");
+  };
   return (
-    <div>
-      <h2> Old Monkey Please Login</h2>
+    <div className="container w-50 mx-auto border mt-5 shadow border-light">
+      <h2 className="text-primary text-center mt-3">Please Login</h2>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            ref={emailRef}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            ref={passwordRef}
+            type="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+        <Button onClick={handleSubmit} variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      <p className="mt-2">
+        Don't have an account?
+        <Link
+          to="/register"
+          className="text-danger text-decoration-none"
+          onClick={navigateRegister}
+        >
+          Please Register
+        </Link>
+      </p>
     </div>
   );
 };
-
 export default Login;
