bfacecdf935e4424a3573c6b7196d5553ade2422
MrRonnie <ronymaza55@gmail.com>
2022-05-09 13:52:09 +0600

    implimant google login

--- src/components/Login/SocialLogin/SocialLogin.js
+++ src/components/Login/SocialLogin/SocialLogin.js
@@ -1,14 +1,48 @@
 import React from "react";
+import google from "../../../images/social/google.png";
+import github from "../../../images/social/github.png";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
+import { useNavigate } from "react-router-dom";
 
 const SocialLogin = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const navigate = useNavigate();
+  let errorElement;
+  if (error) {
+    errorElement = (
+      <div>
+        <p className="text-danger">Error: {error.message}</p>
+      </div>
+    );
+  }
+
+  if (user) {
+    navigate("/home");
+  }
   return (
     <div>
       <div className="d-flex align-items-center">
-        <div style={{ height: "1px" }} className="bg-dark w-50"></div>
-        <p className="mt-2 px-2">or</p>
-        <div style={{ height: "1px" }} className="bg-dark w-50"></div>
+        <div style={{ height: "1px" }} className="bg-info w-50"></div>
+        <p style={{ color: "gray" }} className="mt-2 px-2">
+          or
+        </p>
+        <div style={{ height: "1px" }} className="bg-info w-50"></div>
       </div>
-      <div className="btn btn-primary w-50">Google Sign In</div>
+      {errorElement}
+      <div>
+        <button
+          onClick={() => signInWithGoogle()}
+          className="btn btn-light border-info shadow-lg rounded mb-2 w-50 d-block mx-auto"
+        >
+          <img className="mb-1" src={google} alt="" />
+          <span className="px-2"> Google Login</span>
+        </button>
+        <button className="btn btn-light border-info shadow-lg rounded mb-2 w-50 d-block mx-auto">
+          <img className="mb-1" src={github} alt="" />
+          <span className="px-2"> Github Login</span>
+        </button>
+      </div>
     </div>
   );
 };
